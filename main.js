b9d668ef11e63a9dd9cf028e793fb4222094e288
Adèle Jouneau <121813874+Adelejouneau@users.noreply.github.com>
2023-04-29 17:14:56 +0200

    localstorage

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 30,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
 // Récupération des élements html
 var modal = document.querySelector('#laModale');
 var inputTitre = document.querySelector('#titre');
@@ -15,16 +17,32 @@ var vegan = document.querySelector('input[id="resto-vegan"]');
 var sansGluten = document.querySelector('input[id="resto-sansgluten"]');
 var sansLactose = document.querySelector('input[id="resto-sanslactose"]');
 var coordonnée
-var tableauMarker
+
+
+var tableauMarker = [];
+
 try {
-    // on essaye de récupérer le tableau dans le localstorage
-    tableauMarker = JSON.parse(localStorage.getItem('savetableauMarker')) || [];
+      // on essaye de récupérer le tableau dans le localstorage
+  var storageData = localStorage.getItem('savetableauMarker_v2');
+  if (storageData) {
+    tableauMarker = JSON.parse(storageData);
+  }
+} catch (error) {
+      // si ça ne fonctionne pas, on crée un tableau vide
+  console.error('Erreur lors de la récupération des données du localstorage :', error);
 }
-catch (error) {
-    // si ça ne fonctionne pas, on crée un tableau vide
-    tableauMarker = [];
-    // et on ne fait rien avec l'erreur
-}
+
+// var tableauMarker
+// try {
+//     // on essaye de récupérer le tableau dans le localstorage
+//     tableauMarker = JSON.parse(localStorage.getItem('savetableauMarker')) || [];
+// }
+// catch (error) {
+//     // si ça ne fonctionne pas, on crée un tableau vide
+//     tableauMarker = [];
+//     // et on ne fait rien avec l'erreur
+// }
+
 function onMapClick(e) {
     coordonnée= e.latlng;
     modal.showModal();
@@ -45,10 +63,14 @@ modal.addEventListener('close', function () {
         ajoutMarkerSurLaMap(inputTitre.value, inputAdresse.value, inputSite.value, inputDescription.value, coordonnée);
     }
 });
+
+
 // on charge les marqueurs du localstorage
 for (var i = 0; i < tableauMarker.length; i++) {
     ajoutMarkerSurLaMap(tableauMarker[i].titre, tableauMarker[i].adresse, tableauMarker[i].site, tableauMarker[i].description,  tableauMarker[i].coordonnée);
 }
+
+
 function ajoutMarkerSurLaMap(titre, adresse, site, description, coordonnée) {
     var marker = new L.Marker([coordonnée.lat, coordonnée.lng]).addTo(map);
     marker.bindPopup(
@@ -60,6 +82,8 @@ function ajoutMarkerSurLaMap(titre, adresse, site, description, coordonnée) {
         // + '<img src="' + image + '" alt="' + titre + '">'
     );
 }
+
+
 function supprimeMarker(lat, lng) {
     map.eachLayer(function (layer) {
         if (layer instanceof L.Marker) {
@@ -68,6 +92,8 @@ function supprimeMarker(lat, lng) {
             }
         }
     });
+
+
     tableauMarker = tableauMarker.filter(function (marker) {
         return marker.coordonnée.lat !== lat || marker.coordonnée.lng !== lng;
     });
